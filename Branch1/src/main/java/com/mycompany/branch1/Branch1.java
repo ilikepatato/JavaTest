73cce3970840064e97bf22e5b22bdfd1d6c4e6d4
user <user@DESKTOP-CQOHS3O>
2024-03-17 03:57:43 +0800

    Updated with branch1 text

---
+++ Branch1/src/main/java/com/mycompany/branch1/Branch1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.branch1;
+
+/**
+ *
+ * @author user
+ */
+public class Branch1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Branch1");
+    }
+}
