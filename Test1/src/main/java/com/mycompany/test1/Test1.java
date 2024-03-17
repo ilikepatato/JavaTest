e9eb98fb652163fd50f412c4223a0f7a47af3336
user <user@DESKTOP-CQOHS3O>
2024-03-17 13:31:57 +0800

    Updated with branch1 text

--- Test1/src/main/java/com/mycompany/test1/Test1.java
+++ Test1/src/main/java/com/mycompany/test1/Test1.java
@@ -11,7 +11,10 @@ package com.mycompany.test1;
 public class Test1 {
 
     public static void main(String[] args) {
+        System.out.println("3");
+
+        
+        
         System.out.println("Hello World!");
-        System.out.println("2");
     }
 }
