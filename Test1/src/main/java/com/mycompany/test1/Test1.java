9bf82d1ca70ed8aee0dd404e22318017e7dc367a
ilikepatato <94219964+ilikepatato@users.noreply.github.com>
2024-03-17 13:27:25 +0800

    Update Test1.java

--- Test1/src/main/java/com/mycompany/test1/Test1.java
+++ Test1/src/main/java/com/mycompany/test1/Test1.java
@@ -11,6 +11,8 @@ package com.mycompany.test1;
 public class Test1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("3");
+        System.out.println("2");
+        System.out.println("1");
     }
 }
